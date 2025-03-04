6856f43380b5d2ea747619e2051f32f56227f429
Victor Alfonso Martínez Toscano <141877629+VictorMtMwr@users.noreply.github.com>
2025-03-04 08:57:58 -0500

    Add files via upload
    Se crea la vista 'Mi perfil'

--- js/util/changeLanguage.js
+++ js/util/changeLanguage.js
@@ -1,9 +1,11 @@
-document.getElementById('change-language-btn').addEventListener('click', () => {
+document.getElementById('language-toggle').addEventListener('click', () => {
   const currentLang = document.documentElement.lang;
   const newLang = currentLang === 'es' ? 'en' : 'es';
   document.documentElement.lang = newLang;
   // Update text content based on the new language
   updateTextContent(newLang);
+  // Update button text
+  document.getElementById('language-toggle').textContent = newLang === 'es' ? 'ESPAÑOL' : 'ENGLISH';
 });
 
 function updateTextContent(lang) {
@@ -16,39 +18,23 @@ function updateTextContent(lang) {
 
 const translations = {
   es: {
-    'header-title': 'Bienvenido de Nuevo Andres!',
-    'destino-h2': 'Tu último destino',
-    'ultimo-destino': 'Playa',
-    'proxima-inspeccion-h2': '¿Cual sera tu proxima aventura?',
-    'posibles-destinos-h2': 'Posibles destinos',
-    'destino-montana': 'Montaña',
-    'destino-playa': 'Playa',
-    'destino-nieve': 'Nieve',
+    'ajustes-title': 'Ajustes',
+    'ajustes-h2': 'Ajustes',
+    'idioma-label': 'Idioma',
+    'modo-claro-label': 'Modo claro',
     'inicio': 'Inicio',
     'ajustes': 'Ajustes',
     'mi-perfil': 'Mi Perfil',
-    'modal-title': '¿Qué país quiere visitar?',
-    'label-country': 'Seleccione un país:',
-    'btn-enviar': 'Enviar',
-    'ajustes-h2': 'Ajustes',
-    'change-language-btn': 'Cambiar Idioma'
+    'change-language-btn': 'ESPAÑOL'
   },
   en: {
-    'header-title': 'Welcome Back Andres!',
-    'destino-h2': 'Your last destination',
-    'ultimo-destino': 'Beach',
-    'proxima-inspeccion-h2': 'What will be your next adventure?',
-    'posibles-destinos-h2': 'Possible destinations',
-    'destino-montana': 'Mountain',
-    'destino-playa': 'Beach',
-    'destino-nieve': 'Snow',
+    'ajustes-title': 'Settings',
+    'ajustes-h2': 'Settings',
+    'idioma-label': 'Language',
+    'modo-claro-label': 'Light mode',
     'inicio': 'Home',
     'ajustes': 'Settings',
     'mi-perfil': 'My Profile',
-    'modal-title': 'Which country do you want to visit?',
-    'label-country': 'Select a country:',
-    'btn-enviar': 'Send',
-    'ajustes-h2': 'Settings',
-    'change-language-btn': 'Change Language'
+    'change-language-btn': 'ENGLISH'
   }
 };
